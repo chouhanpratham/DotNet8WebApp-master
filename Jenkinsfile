52cb8d2796ff28fca5b2c83fe11198a6a432013f
Pratham Chouhan <109415931+chouhanpratham@users.noreply.github.com>
2025-04-03 19:14:21 +0530

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+    environment {
+        AZURE_CREDENTIALS_ID = 'azure-service-principal'
+        RESOURCE_GROUP = 'rg-jenkins'
+        APP_SERVICE_NAME = 'webapijenkinspratham22025'
+    }
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch: 'master', url: 'https://github.com/TanishqJecrc/DotNet8WebApp.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'dotnet restore'
+                sh 'dotnet build --configuration Release'
+                sh 'dotnet publish -c Release -o ./publish'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                withCredentials([azureServicePrincipal(credentialsId: AZURE_CREDENTIALS_ID)]) {
+                    sh "az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID"
+                    sh "powershell Compress-Archive -Path ./publish/* -DestinationPath ./publish.zip -Force"
+                    sh "az webapp deploy --resource-group $RESOURCE_GROUP --name $APP_SERVICE_NAME --src-path ./publish.zip --type zip"
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Deployment Successful!'
+        }
+        failure {
+            echo 'Deployment Failed!'
+        }
+    }
+}
---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> employees = new List<Employee>() {
+            new Employee {Id = 1, Name = "Somay Verma" , Job = "CEO", Salary = 912394
+            }
+        };
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee([FromBody] Employee employeeobj)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            employees.Add(employeeobj);
+
+            return Ok("New Employee is added to Company Server");
+        }
+
+        [HttpPut]
+        public IActionResult EditEmployee([FromBody] Employee employeeobj)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+            foreach(Employee employee in employees)
+            {
+                if (employee.Id == employeeobj.Id){
+                    employee.Id = employeeobj.Id;
+                    employee.Name = employeeobj.Name;
+                    employee.Job = employeeobj.Job;
+                    employee.Salary = employeeobj.Salary;
+                }
+            }
+            return Ok("Employee  Details Edited");
+        }
+    }
+}
